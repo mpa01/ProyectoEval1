339994457916de1f7b48c87df0e61b17ab567c8b
mpa01 <mpa01@iesemilidarder.com>
2024-12-02 18:47:04 +0100

    Add files via upload

---
+++ ProyectoEval1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoeval1;
+
+/**
+ *
+ * @author pascu
+ */
+public class ProyectoEval1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hello World0");
+    }
+    
+}
